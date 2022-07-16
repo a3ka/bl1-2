3b99549bcc0f859fe29a21940619c8a85ab28db7
Oleksandr Kalynovskyy <alex.kurz@protonmail.com>
2022-07-16 15:14:17 +0300

    added second endpoint - posts

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,49 @@ const bloggers = [
     {id: 5, name: 'Lena', youtubeUrl: 'https://www.lena.com'},
 ]
 
+const posts = [
+    {
+        id: 1,
+        title: '1st Post',
+        shortDescription: 'Description of 1st post',
+        content: 'Content of 1st Post',
+        bloggerId: 1,
+        bloggerName: 'Alex'
+    },
+    {
+        id: 2,
+        title: '2nd Post',
+        shortDescription: 'Description of 2 post',
+        content: 'Content of 2 Post',
+        bloggerId: 2,
+        bloggerName: 'Sasha'
+    },
+    {
+        id: 3,
+        title: '3 Post',
+        shortDescription: 'Description of 3 post',
+        content: 'Content of 3 Post',
+        bloggerId: 3,
+        bloggerName: 'Serg'
+    },
+    {
+        id: 4,
+        title: '4s Post',
+        shortDescription: 'Description of 4 post',
+        content: 'Content of 4 Post',
+        bloggerId: 4,
+        bloggerName: 'Masha'
+    },
+    {
+        id: 5,
+        title: '5s Post',
+        shortDescription: 'Description of 5 post',
+        content: 'Content of 5 Post',
+        bloggerId: 5,
+        bloggerName: 'Lena'
+    },
+]
+
 
 const app = express()
 app.use(cors())
@@ -41,12 +84,11 @@ app.get('/bloggers/:bloggerId', (req: Request, res: Response) => {
 app.post('/bloggers', (req: Request, res: Response) => {
 
     let name = req.body.name
-    let youtubeUrl= req.body.youtubeUrl
+    let youtubeUrl = req.body.youtubeUrl
     let pattern = /^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/
     let youtubeUrlChecked = pattern.test(youtubeUrl)
 
-    // if (!name || typeof name !== 'string' || typeof youtubeUrl !== 'string' || !name.trim() ||  name.length > 15
-    if (!name || typeof name !== 'string' || typeof youtubeUrl !== 'string' || !name.trim() ||  name.length > 15
+    if (!name || typeof name !== 'string' || typeof youtubeUrl !== 'string' || !name.trim() || name.length > 15
         || youtubeUrl.length > 100 || youtubeUrlChecked === false) {
         res.status(400).send({
             "errorsMessages": [
@@ -60,8 +102,8 @@ app.post('/bloggers', (req: Request, res: Response) => {
     } else {
         const newBlogger = {
             id: +(new Date()),
-            name: req.body.name,
-            youtubeUrl: req.body.youtubeUrl
+            name: name,
+            youtubeUrl: youtubeUrl
         }
         bloggers.push(newBlogger)
         res.status(201).send(newBlogger)
@@ -85,11 +127,11 @@ app.delete('/bloggers/:bloggerId', (req: Request, res: Response) => {
 app.put('/bloggers/:bloggerId', (req: Request, res: Response) => {
 
     let name = req.body.name
-    let youtubeUrl= req.body.youtubeUrl
+    let youtubeUrl = req.body.youtubeUrl
     let pattern = /^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/
     let youtubeUrlChecked = pattern.test(youtubeUrl)
 
-    if (!name || typeof name !== 'string' || typeof youtubeUrl !== 'string' || !name.trim() ||  name.length > 15
+    if (!name || typeof name !== 'string' || typeof youtubeUrl !== 'string' || !name.trim() || name.length > 15
         || youtubeUrl.length > 100 || youtubeUrlChecked === false) {
         res.status(400).send({
             "errorsMessages": [
@@ -108,13 +150,130 @@ app.put('/bloggers/:bloggerId', (req: Request, res: Response) => {
         if (video) {
             video.name = req.body.name
             video.youtubeUrl = req.body.youtubeUrl
-            // res.status(204).send(video);
-            res.send(204);
+            res.status(204).send(video);
+            // res.send(204);
         } else {
             res.send(404)
         }
     }
 })
+
+/// POSTS !!!!!
+
+
+app.get('/posts', (req: Request, res: Response) => {
+    res.status(200).send(posts);
+})
+
+app.get('/posts/:postId', (req: Request, res: Response) => {
+
+    if (typeof +req.params.postId !== "number") {
+        res.send(400);
+        return;
+    }
+
+    const post = posts.find(p => p.id === +req.params.postId);
+
+    if (post) {
+        res.status(200).send(post);
+    } else {
+        res.send(404);
+    }
+})
+
+
+app.post('/posts', (req: Request, res: Response) => {
+
+    let title = req.body.title
+    let shortDescription = req.body.shortDescription
+    let content = req.body.content
+    let bloggerId = req.body.bloggerId
+    // let bloggerName = req.body.bloggerName
+
+    if (!title || typeof title !== 'string' || title.length > 30
+        || !shortDescription || typeof shortDescription !== 'string' || shortDescription.length > 100
+        || !content || typeof content !== 'string' || content.length > 1000
+        || !bloggerId || typeof bloggerId !== "number"
+    ) {
+
+        res.status(400).send({
+            "errorsMessages": [
+                {
+                    "message": "message",
+                    "field": "message"
+                }
+            ]
+        })
+        return;
+
+    } else {
+        const newPost = {
+            id: +(new Date()),
+            title,
+            shortDescription,
+            content,
+            bloggerId,
+            bloggerName: 'Nikol'
+        }
+        posts.push(newPost)
+        res.status(201).send(newPost)
+    }
+})
+
+app.delete('/posts/:postId', (req: Request, res: Response) => {
+
+    for (let i = 0; i < posts.length; i++) {
+        if (posts[i].id === +req.params.postId) {
+            posts.splice(i, 1);
+            res.send(204)
+            return;
+        }
+    }
+
+    res.send(404)
+})
+
+
+app.put('/posts/:postId', (req: Request, res: Response) => {
+
+    let title = req.body.title
+    let shortDescription = req.body.shortDescription
+    let content = req.body.content
+    let bloggerId = req.body.bloggerId
+    let bloggerName = req.body.bloggerName
+
+    if (!title || typeof title !== 'string' || title.length > 30 ||
+        !shortDescription || typeof shortDescription !== 'string' || shortDescription.length > 100 ||
+        !content || typeof content !== 'string' || content.length > 1000 ||
+        !bloggerId || typeof bloggerId !== 'number') {
+
+        res.status(400).send({
+            "errorsMessages": [
+                {
+                    "message": "message",
+                    "field": "message"
+                }
+            ]
+        })
+        return;
+
+    } else {
+
+        const blogPost = posts.find(p => p.id === +req.params.postId);
+
+        if (blogPost) {
+            blogPost.title = title
+            blogPost.shortDescription = shortDescription
+            blogPost.content = content
+            blogPost.bloggerId = bloggerId
+            res.status(204).send(blogPost);
+            // res.send(204);
+        } else {
+            res.send(404)
+        }
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
