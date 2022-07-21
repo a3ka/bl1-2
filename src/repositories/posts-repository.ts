8bda31ff42fc91d9e670868b7dd417a1aa6b58f4
Oleksandr Kalynovskyy <alex.kurz@protonmail.com>
2022-07-21 23:47:03 +0300

    DataAccessLayer - Repositories

---
+++ src/repositories/posts-repository.ts
@@ -0,0 +1,125 @@
+const bloggers = [
+    {id: 1, name: 'Alex', youtubeUrl: 'https://www.alex.com'},
+    {id: 2, name: 'Sasha', youtubeUrl: 'https://www.sasha.com'},
+    {id: 3, name: 'Serg', youtubeUrl: 'https://www.serg.com'},
+    {id: 4, name: 'Masha', youtubeUrl: 'https://www.masha.com'},
+    {id: 5, name: 'Lena', youtubeUrl: 'https://www.lena.com'},
+]
+
+const posts:PostType[] = [
+    {
+        id: 1,
+        title: '1st Post',
+        shortDescription: 'Description of 1st post',
+        content: 'Content of 1st Post',
+        bloggerId: 1,
+        bloggerName: 'Alex'
+    },
+    {
+        id: 2,
+        title: '2nd Post',
+        shortDescription: 'Description of 2 post',
+        content: 'Content of 2 Post',
+        bloggerId: 2,
+        bloggerName: 'Sasha'
+    },
+    {
+        id: 3,
+        title: '3 Post',
+        shortDescription: 'Description of 3 post',
+        content: 'Content of 3 Post',
+        bloggerId: 3,
+        bloggerName: 'Serg'
+    },
+    {
+        id: 4,
+        title: '4s Post',
+        shortDescription: 'Description of 4 post',
+        content: 'Content of 4 Post',
+        bloggerId: 4,
+        bloggerName: 'Masha'
+    },
+    {
+        id: 5,
+        title: '5s Post',
+        shortDescription: 'Description of 5 post',
+        content: 'Content of 5 Post',
+        bloggerId: 5,
+        bloggerName: 'Lena'
+    },
+]
+
+type PostType = {
+    id: number
+    title: string
+    shortDescription: string
+    content: string
+    bloggerId: number
+    bloggerName: string
+}
+
+
+
+export const postsRepository = {
+    getAllPosts () {
+        return posts;
+    },
+
+    createPost (title: string, shortDescription: string, content: string, bloggerId: number) {
+        const blogger = bloggers.find(b => b.id === +bloggerId);
+        if (blogger) {
+            const newPost = {
+                id: +(new Date()),
+                title,
+                shortDescription,
+                content,
+                bloggerId,
+                bloggerName: blogger.name
+            }
+            posts.push(newPost)
+            return newPost
+        }
+    },
+
+    getPostById (postId: number) {
+        const post = posts.find(p => p.id === postId);
+        return post;
+    },
+
+    isBlogger (bloggerId: number) {
+        const blogger = bloggers.find(b => b.id === bloggerId);
+
+        if (blogger) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    updatePost (postId: number, title: string, shortDescription: string, content: string, bloggerId: number) {
+
+        const blogPost = posts.find(p => p.id === postId);
+
+        if (blogPost) {
+            blogPost.title = title
+            blogPost.shortDescription = shortDescription
+            blogPost.content = content
+            blogPost.bloggerId = bloggerId
+            // blogPost.bloggerName = blogPost.bloggerName
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    deletePost (postId: number) {
+        for (let i = 0; i < posts.length; i++) {
+            if (posts[i].id === postId) {
+                posts.splice(i, 1);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
--- src/routers/posts-router.ts
+++ src/routers/posts-router.ts
@@ -1,93 +1,28 @@
 import {Request, Response, Router} from "express";
+import {postsRepository} from "../repositories/posts-repository";
 
-const bloggers = [
-    {id: 1, name: 'Alex', youtubeUrl: 'https://www.alex.com'},
-    {id: 2, name: 'Sasha', youtubeUrl: 'https://www.sasha.com'},
-    {id: 3, name: 'Serg', youtubeUrl: 'https://www.serg.com'},
-    {id: 4, name: 'Masha', youtubeUrl: 'https://www.masha.com'},
-    {id: 5, name: 'Lena', youtubeUrl: 'https://www.lena.com'},
-]
-
-const posts:PostType[] = [
-    {
-        id: 1,
-        title: '1st Post',
-        shortDescription: 'Description of 1st post',
-        content: 'Content of 1st Post',
-        bloggerId: 1,
-        bloggerName: 'Alex'
-    },
-    {
-        id: 2,
-        title: '2nd Post',
-        shortDescription: 'Description of 2 post',
-        content: 'Content of 2 Post',
-        bloggerId: 2,
-        bloggerName: 'Sasha'
-    },
-    {
-        id: 3,
-        title: '3 Post',
-        shortDescription: 'Description of 3 post',
-        content: 'Content of 3 Post',
-        bloggerId: 3,
-        bloggerName: 'Serg'
-    },
-    {
-        id: 4,
-        title: '4s Post',
-        shortDescription: 'Description of 4 post',
-        content: 'Content of 4 Post',
-        bloggerId: 4,
-        bloggerName: 'Masha'
-    },
-    {
-        id: 5,
-        title: '5s Post',
-        shortDescription: 'Description of 5 post',
-        content: 'Content of 5 Post',
-        bloggerId: 5,
-        bloggerName: 'Lena'
-    },
-]
-
-type ErrorsMessagesType = {
-    message: string
-    field: string | null
-}
-
-type PostType = {
-    id: number
-    title: string
-    shortDescription: string
-    content: string
-    bloggerId: number
-    bloggerName: string
-}
-
-type ErrorResponseType = { errorsMessages:ErrorsMessagesType[] }
 
 export const postsRouter = Router({});
 
 
 postsRouter.get('/', (req: Request, res: Response) => {
-    // res.status(200).send(postMapper(posts));
+    const posts = postsRepository.getAllPosts()
     res.status(200).send(posts);
 })
 
 postsRouter.post('/', (req: Request, res: Response) => {
 
-    let title = req.body.title
-    let shortDescription = req.body.shortDescription
-    let content = req.body.content
-    let bloggerId = req.body.bloggerId
+    const title = req.body.title
+    const shortDescription = req.body.shortDescription
+    const content = req.body.content
+    const bloggerId = req.body.bloggerId
 
-    let titleErrors = !title || typeof title !== 'string' || title.trim().length === 0 || title.trim().length > 30
-    let shortDescriptionErrors = !shortDescription || typeof shortDescription !== 'string' || shortDescription.length > 100
-    let contentErrors = !content || typeof content !== 'string' || content.trim().length === 0 || content.trim().length > 1000
-    let bloggerIdErrors = !bloggerId || typeof bloggerId !== "number"
+    const titleErrors = !title || typeof title !== 'string' || title.trim().length === 0 || title.trim().length > 30
+    const shortDescriptionErrors = !shortDescription || typeof shortDescription !== 'string' || shortDescription.length > 100
+    const contentErrors = !content || typeof content !== 'string' || content.trim().length === 0 || content.trim().length > 1000
+    const bloggerIdErrors = !bloggerId || typeof bloggerId !== "number"
 
-    let errorsMessages: ErrorResponseType = { errorsMessages: [] }
+    const errorsMessages: ErrorResponseType = {errorsMessages: []}
 
     if (titleErrors || shortDescriptionErrors || contentErrors || bloggerIdErrors) {
 
@@ -96,7 +31,10 @@ postsRouter.post('/', (req: Request, res: Response) => {
         }
 
         if (shortDescriptionErrors) {
-            errorsMessages.errorsMessages.push({message: "Problem with a shortDescription field",  field: "shortDescription" })
+            errorsMessages.errorsMessages.push({
+                message: "Problem with a shortDescription field",
+                field: "shortDescription"
+            })
         }
 
         if (contentErrors) {
@@ -108,21 +46,10 @@ postsRouter.post('/', (req: Request, res: Response) => {
         }
 
         res.status(400).send(errorsMessages)
-        return;
 
     } else {
-        const blogger = bloggers.find(b => b.id === +bloggerId);
-        if (blogger) {
-            const newPost = {
-                id: +(new Date()),
-                title,
-                shortDescription,
-                content,
-                bloggerId,
-                bloggerName: blogger.name
-            }
-
-            posts.push(newPost)
+        if (true) {
+            const newPost = postsRepository.createPost(title, shortDescription, content, bloggerId)
             res.status(201).send(newPost)
         } else {
             errorsMessages.errorsMessages.push({message: "Blogger not found", field: "bloggerId"})
@@ -138,15 +65,9 @@ postsRouter.get('/:postId', (req: Request, res: Response) => {
         return;
     }
 
-    const post = posts.find(p => p.id === +req.params.postId);
-    // const post = []
-    // post.push(posts.find(p => p.id === +req.params.postId))
-
-    // @ts-ignore
-    // delete post.bloggerName
+    const post = postsRepository.getPostById(+req.params.postId)
 
     if (post) {
-        // res.status(200).send(post);
         res.status(200).send(post);
     } else {
         res.send(404);
@@ -155,15 +76,13 @@ postsRouter.get('/:postId', (req: Request, res: Response) => {
 
 postsRouter.delete('/:postId', (req: Request, res: Response) => {
 
-    for (let i = 0; i < posts.length; i++) {
-        if (posts[i].id === +req.params.postId) {
-            posts.splice(i, 1);
-            res.send(204)
-            return;
-        }
-    }
+    const isDeleted = postsRepository.deletePost(+req.params.postId)
 
-    res.send(404)
+    if(isDeleted){
+        res.send(204)
+    } else {
+        res.send(404)
+    }
 })
 
 postsRouter.put('/:postId', (req: Request, res: Response) => {
@@ -178,19 +97,21 @@ postsRouter.put('/:postId', (req: Request, res: Response) => {
     let contentErrors = !content || typeof content !== 'string' || content.trim().length === 0 || content.trim().length > 1000
     let bloggerIdErrors = !bloggerId || typeof bloggerId !== "number"
 
-    // const blogPost = posts.find(p => p.id === +req.params.postId);
-    const blogger = bloggers.find(b => b.id === bloggerId);
-    // console.log(blogger)
-    if (titleErrors || shortDescriptionErrors || contentErrors || bloggerIdErrors || !blogger ) {
+    const blogger = postsRepository.isBlogger(bloggerId);
 
-        let errorsMessages: ErrorResponseType = { errorsMessages: [] }
+    if (titleErrors || shortDescriptionErrors || contentErrors || bloggerIdErrors || !blogger) {
+
+        let errorsMessages: ErrorResponseType = {errorsMessages: []}
 
         if (titleErrors) {
             errorsMessages.errorsMessages.push({message: "Problem with a Title field", field: "title"})
         }
 
         if (shortDescriptionErrors) {
-            errorsMessages.errorsMessages.push({message: "Problem with a shortDescription field", field: "shortDescription"})
+            errorsMessages.errorsMessages.push({
+                message: "Problem with a shortDescription field",
+                field: "shortDescription"
+            })
         }
 
         if (contentErrors) {
@@ -206,13 +127,10 @@ postsRouter.put('/:postId', (req: Request, res: Response) => {
 
     } else {
 
-        const blogPost = posts.find(p => p.id === +req.params.postId);
-        if (blogPost) {
-            blogPost.title = title
-            blogPost.shortDescription = shortDescription
-            blogPost.content = content
-            blogPost.bloggerId = bloggerId
-            // blogPost.bloggerName = blogPost.bloggerName
+        const isUpdated = postsRepository.updatePost(+req.params.postId, title, shortDescription, content, bloggerId)
+
+        if (isUpdated) {
+            const blogPost = postsRepository.getPostById(+req.params.postId)
             res.status(204).send(blogPost);
             // res.send(204);
         } else {
@@ -221,3 +139,9 @@ postsRouter.put('/:postId', (req: Request, res: Response) => {
     }
 })
 
+type ErrorsMessagesType = {
+    message: string
+    field: string | null
+}
+
+type ErrorResponseType = { errorsMessages: ErrorsMessagesType[] }
