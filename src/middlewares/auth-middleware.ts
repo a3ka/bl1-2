d109b4d029696d3d4746dda70c12ecfae75603e5
Oleksandr Kalynovskyy <alex.kurz@protonmail.com>
2022-07-22 21:58:31 +0300

    test#2

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -6,7 +6,7 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
     const header = req.headers.authorization
     if (header === 'Basic YWRtaW46cXdlcnR5') {
         next()
-        // return
+        return
     }
     res.send(401)
 }
