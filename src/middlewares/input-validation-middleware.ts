aa850c5608b0eb1a2239231489a972a35b402831
Oleksandr Kalynovskyy <alex.kurz@protonmail.com>
2022-07-23 18:18:44 +0300

    test#10: work on style of ErrorMessage

--- src/middlewares/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware.ts
@@ -2,10 +2,31 @@ import {NextFunction, Request, Response} from "express";
 import {validationResult} from "express-validator";
 
 export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
+    const errors:any = validationResult(req);
+    let errorsMessages:any =  []
 
-        res.status(400).json({ errorsMessages: errors.array() });
+    for (let i = 0; i < errors.length; i++) {
+        Object.keys(errors[i]).forEach(key => {
+            let value = errors[0][key];
+
+            errorsMessages[0].push({})
+
+            if(key === 'msg') {
+                errorsMessages[0].message = value
+            } else if (key === 'param') {
+                errorsMessages[0].field = value
+            } else {
+                return
+            }
+
+            // console.log(`${key}: ${value}`);
+        });
+    }
+
+
+    if (!errors.isEmpty()) {
+        // res.status(400).json({ errorsMessages: errors.array() });
+        res.status(400).json({ errorsMessages: errorsMessages.array() });
     } else {
         next()
     }
