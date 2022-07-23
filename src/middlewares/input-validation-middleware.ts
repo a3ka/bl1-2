95260aa7b67b6a03496dd4826f314f51aa63b966
Oleksandr Kalynovskyy <alex.kurz@protonmail.com>
2022-07-23 17:19:40 +0300

    test#9

--- src/middlewares/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware.ts
@@ -5,8 +5,7 @@ export const inputValidationMiddleware = (req: Request, res: Response, next: Nex
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
 
-        // res.status(400).json({ errors: errors.array() });
-        res.status(400).json({ errors: errors});
+        res.status(400).json({ errorsMessages: errors.array() });
     } else {
         next()
     }
