53f56842643c04ecce6ffc271654f313ff88b120
Oleksandr Kalynovskyy <alex.kurz@protonmail.com>
2022-07-23 17:04:45 +0300

    test#7

--- src/middlewares/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware.ts
@@ -4,7 +4,17 @@ import {validationResult} from "express-validator";
 export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() });
+
+        let errorsMessages = {errorsMessages: []}
+        // @ts-ignore
+        // let errors = errors.array()
+
+        let errors = res.json({ errors: errors.array() })
+
+        console.log('ERROR HERE!!!!' + errors)
+
+        res.status(400).json(errors);
+        // res.status(400).json({ errors: errors.array() });
     } else {
         next()
     }
